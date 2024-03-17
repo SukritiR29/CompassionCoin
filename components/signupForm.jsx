06e47d89fac2ff704739d810a52e840793d3b346
SukritiR29 <sukritirajora@gmail.com>
2024-03-17 09:45:20 +0530

    signup form

--- components/signupForm.jsx
+++ components/signupForm.jsx
@@ -10,6 +10,7 @@ const SignupForm = () => {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [role, setRole] = useState("")
     const [error, setError] = useState("")
 
     const router = useRouter();
@@ -17,7 +18,7 @@ const SignupForm = () => {
     const handleSubmit = async(e) => {
         e.preventDefault();
 
-        if(!name || !email || !password){
+        if(!name || !email || !password || !role){
             setError("All fields are necessary!");
             console.log("Error.")
             return;
@@ -50,12 +51,13 @@ const SignupForm = () => {
                 name,
                 email,
                 password,
+                role,
             }),
            });
            if(res.ok) {
             const form = e.target;
             form.reset();
-            router.push("/login")
+            router.push("/dashboard")
            } else {
             console.log("Registration failed.");
            }
@@ -66,36 +68,74 @@ const SignupForm = () => {
     
 
   return (
-    <div className=' p-5 pr-10 pl-10 justify-end rounded-xl bg-slate-900 bg-opacity-50'>
+    <div className=' pr-10 pl-10 justify-end rounded-xl bg-slate-900 bg-opacity-50'>
         <div className='text-slate-300'>
-            <h1 className='text-slate-300 text-4xl text-center font-sans mb-2'>Easels  AI</h1>
-            <p className='text-xs text-center '>AI art by the people, for the people.</p>
+            <h1 className='text-slate-300 text-3xl text-center font-sans mb-2'>Compassion Coin</h1>
+            {/* <p className='text-xs text-center '>AI art by the people, for the people.</p> */}
         </div>
         <div className='p-3 text-slate-300'>
             <h2 className='text-lg mb-1 text-center'>Sign Up</h2>
             <div className='w-max ml-5 mr-5'>
             <form onSubmit={handleSubmit}>
+            
+            
             <p className='text-sm'>Name:</p>
             <input onChange={(e) => setName(e.target.value)}
              type="text" 
              placeholder='Enter Name'
-             className='bg-transparent border border-slate-400 rounded-lg mt-1 mb-3 p-1 pl-3 text-sm w-max' />
+             className='bg-transparent border border-slate-400 rounded-lg mt-1 mb-3 p-2 pr-20 text-sm w-max' />
 
             <p className='text-sm'>Email:</p>
             <input onChange={(e) => setEmail(e.target.value)} 
             type="email" name="name" 
             placeholder='Enter Email'
-            className='bg-transparent border border-slate-400 rounded-lg mt-1 mb-3 p-1 pl-3 text-sm ' />
+            className='bg-transparent border border-slate-400 rounded-lg mt-1 mb-3 p-2 pr-20 text-sm ' />
             <p className='text-sm'>Password:</p>
             <input onChange={(e) => setPassword(e.target.value)} 
             type="password" name="name" 
             placeholder='Enter Password'
-            className='bg-transparent border border-slate-400 rounded-lg mt-1 mb-3 p-1 pl-3 text-sm ' />
+            className='bg-transparent border border-slate-400 rounded-lg mt-1 mb-3 p-2 pr-20 text-sm ' />
+            <p className='text-sm'>Join as:</p>
+            <div className='flex gap-5'>
+            <div>
+                <input
+                    type="radio"
+                    id="admin"
+                    name="role"
+                    value="Admin"
+                    onChange={(e) => setRole(e.target.value)}
+                />
+                <label htmlFor="admin">Admin</label>
+            </div>
+            <div>
+                <input
+                    type="radio"
+                    id="user"
+                    name="role"
+                    value="User"
+                    onChange={(e) => setRole(e.target.value)}
+                />
+                <label htmlFor="user">User</label>
+            </div>
 
-            <div className='text-center items-center justify-center mt-4 mb-3'>
-            <button className='bg-pink-600 p-1 mb-2 pr-4 pl-4 text-slate-300 rounded-lg text-sm'>Sign up</button>
+            </div>
+  
+            {/* <div className='flex justify-start gap-10 m-2'>
+                <button className={`bg-blue-100 text-slate-800 p-1 mb-1 pr-4 pl-4 rounded-lg text-sm ${
+                    role === 'Admin' ? "bg-blue-400" : ""
+                }`}
+                onClick={() => setRole("Admin")}>Admin</button>
+                <p>or</p>
+                 <button className={`bg-blue-100 text-slate-800 p-1 mb-1 pr-4 pl-4 rounded-lg text-sm ${
+                    role === 'User' ? "bg-blue-400" : ""
+                }`}
+                onClick={() => setRole("User")}>User</button>
+            </div> */}
+
+            <div className='text-center items-center justify-center mt-8'>
+            <button className='bg-pink-600 p-1 pr-4 pl-4 text-slate-300 rounded-lg text-sm'>Sign up</button>
             {error && (
-            <div className='bg-red-500 p-1 text-sm flex justify-center rounded-md'>
+            <div className='bg-red-500 p-1 text-sm flex justify-center rounded-md mt-3'>
             <IoWarningOutline className='text-lg mr-2 ' />
             {error}
             </div>
