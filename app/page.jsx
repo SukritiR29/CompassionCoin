3ee8999b60c8a96ccd36747dd364fb999ac5c983
SukritiR29 <sukritirajora@gmail.com>
2024-03-17 09:44:49 +0530

    page

--- app/page.jsx
+++ app/page.jsx
@@ -2,23 +2,20 @@ import React from 'react'
 import SignupForm from '@/components/signupForm'
 const page = () => {
 
-  const backgroundImageUrl = 'url("/landing.png")';
-
+  const containerStyle = {
+    backgroundImage: 'url("/office.png")',
+    backgroundSize: '100% 100%',
+    backgroundPosition: 'center',
+  };
   return (
-<div className="hero min-h-screen flex items-center justify-end bg-black" style={{backgroundImage: backgroundImageUrl, backgroundSize: 'cover'}}>
-  <div className="hero-overlay"></div>
-  <div className="hero-content">
-    <div className="max-w-md">
-    <div className='max-h-screen'>
-      <div className='flex justify-end p-20 mr-[100px]'>
-      <SignupForm/>
+    <div className='w-screen h-screen bg-slate-900'>
+      <div className='flex'>
+      <div className='container h-screen w-1/2 bg-slate-800' style={{backgroundImage: 'url("/office2.jpeg")', backgroundSize: '100% 100%', backgroundPosition: 'center', opacity: 0.8 }}></div>
+      <div className='p-16 ml-16'>
+        <SignupForm className='flex justify-center'/>
+      </div>
       </div>
     </div>
-    </div>
-  </div>
-</div>
-
- 
   )
 }
 
