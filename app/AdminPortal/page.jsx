cece3ebfbad6acf2d508a14ae07ab26041c130c2
SukritiR29 <sukritirajora@gmail.com>
2024-03-18 19:24:58 +0530

    admin portal

--- app/AdminPortal/page.jsx
+++ app/AdminPortal/page.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import AdminSide from '@/components/AdminSide'
+import AdminOffer from '@/components/AdminOffer'
 
 const page = () => {
   return (
-    <div>Admin Portal</div>
+    <div className='bg-slate-900 h-screen '>
+      <div className='flex'>
+      <AdminSide className='m-4'/>
+      <AdminOffer/>
+      </div>   
+    </div>
   )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,7 @@ const page = () => {
     <div className='w-screen h-screen bg-slate-900'>
       <div className='flex'>
       <div className='container h-screen w-1/2 bg-slate-800' style={{backgroundImage: 'url("/office2.jpeg")', backgroundSize: '100% 100%', backgroundPosition: 'center', opacity: 0.8 }}></div>
-      <div className='p-16 ml-16'>
+      <div className='p-16 ml-16 mt-4'>
         <SignupForm className='flex justify-center'/>
       </div>
       </div>
