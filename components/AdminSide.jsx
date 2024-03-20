4cfac0d0c57b40f544e7cea5cae0b10813940577
SukritiR29 <sukritirajora@gmail.com>
2024-03-21 01:35:27 +0530

    admin side

--- components/AdminSide.jsx
+++ components/AdminSide.jsx
@@ -18,11 +18,11 @@ const AdminSide = () => {
 
   
   return (
-    <div className='border border-opacity-20 border-slate-200 rounded  w-1/6 p-2 m-4 '>
+    <div className='border border-opacity-20 border-slate-200 rounded  w-1/6 p-2 m-4 ficed  '>
         <div className='text-slate-200 text-sm border p-2 border-slate-200 border-opacity-20 rounded mb-5'>
         <p className='ml-2'>{userName}</p>
         </div>
-      <div className='flex flex-col gap-4'>
+      <div className='flex flex-col gap-4 text-slate-200'>
         <button className='w-full bg-slate-700 text-start text-sm p-2 text-slate-100 rounded'>Offers</button>
         <button className='w-full bg-slate-700 text-start text-sm p-2 text-slate-100 rounded'>Offers</button>
         <button className='w-full bg-slate-700 text-start text-sm p-2 text-slate-100 rounded'>Offers</button>
