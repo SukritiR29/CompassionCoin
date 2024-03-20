3d258a254b4265dd2ba9d05568ebc61e952bd482
SukritiR29 <sukritirajora@gmail.com>
2024-03-21 01:34:32 +0530

    get register

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -3,6 +3,26 @@ import User from "@/models/user";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
+
+export async function GET(req) {
+  try{
+    await connectMongoDB();
+
+    const currUser = await User.find({});
+
+    return NextResponse.json({
+      currUser,
+      success: true,
+    });
+  } catch(error){
+    console.log("Error fetching user:", error);
+
+    return NextResponse.json(
+      {message: "An error occurred while fetching user"},
+      {status: 500}
+    );
+  }
+}
 export async function POST(req) {
   try {
     const { name, email, password, role } = await req.json();
