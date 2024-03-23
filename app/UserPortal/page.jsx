5db2e286a0dd28e3682497575d0b3019c1bb2736
SukritiR29 <sukritirajora@gmail.com>
2024-03-24 00:25:36 +0530

    user portal

--- app/UserPortal/page.jsx
+++ app/UserPortal/page.jsx
@@ -1,12 +1,18 @@
+"use client"
+
 import React from 'react'
 import OfferList from '../../components/userOffer'
+import UserApplication from '@/components/userApplication'
 
 
 const page = () => {
   return (
-    <div className='bg-slate-900 h-max'>
+    <div className='bg-slate-900 h-max flex'>
       <div>
         <OfferList/>
+      </div>
+      <div>
+        <UserApplication/>
       </div>
     </div>
   )
