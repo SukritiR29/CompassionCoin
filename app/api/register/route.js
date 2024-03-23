59f92a891cd025976b34a3e58e9197b2ef2d9239
SukritiR29 <sukritirajora@gmail.com>
2024-03-23 17:56:51 +0530

    register update

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -3,29 +3,31 @@ import User from "@/models/user";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
-
 export async function GET(req) {
-  try{
+  try {
     await connectMongoDB();
 
-    const currUser = await User.find({});
+    const currUser = await User.find(
+      {},
+      { name: 1, email: 1, appliedOffers: 1 }
+    );
 
     return NextResponse.json({
       currUser,
       success: true,
     });
-  } catch(error){
+  } catch (error) {
     console.log("Error fetching user:", error);
 
     return NextResponse.json(
-      {message: "An error occurred while fetching user"},
-      {status: 500}
+      { message: "An error occurred while fetching user" },
+      { status: 500 }
     );
   }
 }
 export async function POST(req) {
   try {
-    const { name, email, password, role } = await req.json();
+    const { name, email, password, role, appliedOffers } = await req.json();
     const hashedPassword = await bcrypt.hash(password, 10);
     await connectMongoDB();
     await User.create({
