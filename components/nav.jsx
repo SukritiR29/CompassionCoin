22a47c90e212b6b006368ff6b5168762c7e0eb3a
SukritiR29 <sukritirajora@gmail.com>
2024-03-25 00:29:27 +0530

    nav bar

---
+++ components/nav.jsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React from 'react'
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { RxAvatar } from "react-icons/rx";
+
+
+
+const Nav = () => {
+    const { data: session, status } = useSession();
+    const [userName, setUserName] = useState('');
+
+    useEffect(() => {
+        if (status === 'authenticated') {
+          setUserName(session.user.name);
+        }
+      }, [session, status]);
+  return (
+    < div className='w-screen pt-2 border border-slate-400 bg-gray-950 border-opacity-20 flex pb-2 flex justify-between'>
+        <h1 className=' pl-6 text-sm font-bold bg-gradient-to-r  from-pink-500  to-yellow-500  text-transparent bg-clip-text uppercase'>Compassion Coin</h1>
+      <div className='flex'>
+      <RxAvatar className='text-md text-slate-200 mr-2'/>
+      <h1 className='mr-10 text-sm font-semibold text-slate-200 uppercase'>{userName}</h1>
+      </div>
+        
+    </div>
+  )
+}
+
+export default Nav
