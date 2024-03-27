9712718c433f400e65b02f3120838236d072d6f4
SukritiR29 <sukritirajora@gmail.com>
2024-03-27 22:36:55 +0530

    admin offer

--- components/AdminOffer.jsx
+++ components/AdminOffer.jsx
@@ -108,16 +108,16 @@ const AdminOffer = () => {
 
   return (
     <div className=''>
-      <form onSubmit={handleSubmit} className="w-full max-w-lg">
-        <div className="flex flex-wrap -mx-3 mb-6">
+      <form onSubmit={handleSubmit} className="w-full mt-20 -ml-20">
+        <div className="flex flex-wrap  mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            <label className="block  uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               Offer Title
             </label>
             <input
               onChange={(e) => setOffer(e.target.value)}
               value={offer}
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              className="appearance-none text-xs block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
               type="text"
               placeholder="Offer Title"
             />
@@ -128,59 +128,63 @@ const AdminOffer = () => {
           </label>
           <input
             value={userEmail}
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            className="appearance-none block text-xs w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
             type="text"
             placeholder="Email"
             readOnly // Prevent users from editing the email field
           />
         </div>
-
-          <div className="w-full md:w-1/2 px-3">
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              Firm Name
+              Firm Name:
             </label>
             <input
               onChange={(e) => setFirm(e.target.value)}
               value={firm}
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none text-xs block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="text"
               placeholder="Firm Name"
             />
           </div>
+         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Worth:
+          </label>
+          <input
+              onChange={(e) => setWorth(e.target.value)}
+              value={worth}
+              className="appearance-none text-xs block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="text"
+              placeholder="Worth"
+            />
         </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
+
+        
+        </div>
+        
+        <div className="flex flex-wrap px-3 -mx-3 mb-6">
           <div className="w-full px-3">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               Description
             </label>
-            <input
+            <textarea
               onChange={(e) => setDescription(e.target.value)}
               value={description}
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none text-xs block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="text"
               placeholder="Description"
             />
-            <p className="text-gray-600 text-xs italic">Make it as long and as crazy as youd like</p>
+            <p className="text-gray-600 text-xs italic">Make it as long and descriptive as youd like</p>
           </div>
         </div>
-        <div className="flex flex-wrap -mx-3 mb-2">
-          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              Worth
-            </label>
-            <input
-              onChange={(e) => setWorth(e.target.value)}
-              value={worth}
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              type="text"
-              placeholder="Worth"
-            />
-          </div>
-          <div className="relative flex">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded justify-end" type="submit">
-              Submit
-            </button>
-          </div>
+        <div className="">
+
+        <div className="flex justify-end">
+  <button className="bg-blue-500 hover:bg-blue-700 h-fit text-white font-semibold  p-2 text-xs rounded justify-center text-center items-center" type="submit">
+    Submit
+  </button>
+</div>
+
         </div>
       </form>
 
