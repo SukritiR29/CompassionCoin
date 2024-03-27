5c983c1c401f9bcc312259953a5e6b1752864ebf
SukritiR29 <sukritirajora@gmail.com>
2024-03-27 22:37:01 +0530

    side bar

--- components/AdminSide.jsx
+++ components/AdminSide.jsx
@@ -15,22 +15,25 @@ const AdminSide = () => {
         }
       }, [session, status]);
 
+      const firstLetter = userName ? userName.charAt(0) : '';
 
   
   return (
-    <div className='border border-opacity-20 border-slate-200 rounded  w-1/6 p-2 m-4 ficed  '>
-        <div className='text-slate-200 text-sm border p-2 border-slate-200 border-opacity-20 rounded mb-5'>
-        <p className='ml-2'>{userName}</p>
+    <div className='border border-opacity-20 border-slate-200 rounded flex-row w-1/6 p-2 h-max  '>
+        <div className='text-slate-200 flex-row  text-sm justify-center items-center text-center p-2 mb-5'>
+        <div className='h-10 w-10 ml-14 m-2 rounded-full bg-purple-600 text-slate-200 flex items-center justify-center bg-gray-300 text-gray-600 uppercase'>
+                {firstLetter}
+            </div>
+        <p className='text-bold text-lg uppercase'>{userName}</p>
         </div>
-      <div className='flex flex-col gap-4 text-slate-200'>
-        <button className='w-full bg-slate-700 text-start text-sm p-2 text-slate-100 rounded'>Offers</button>
-        <button className='w-full bg-slate-700 text-start text-sm p-2 text-slate-100 rounded'>Offers</button>
-        <button className='w-full bg-slate-700 text-start text-sm p-2 text-slate-100 rounded'>Offers</button>
-        <button className='w-full bg-slate-700 text-start text-sm p-2 text-slate-100 rounded'>Offers</button>
-        <button className='w-full bg-slate-700 text-start text-sm p-2 text-slate-100 rounded'>Offers</button>
+      <div className='flex flex-col gap-4  text-slate-200'>
+        <button className='w-full border  border-opacity-20 border-slate-200 text-start text-xs p-2  text-slate-100 rounded'>ABOUT</button>
+        <button className='w-full border border-opacity-20 border-slate-200 text-start text-xs p-2 text-slate-100 rounded'>ROLES</button>
+        <button className='w-full border border-opacity-20 border-slate-200 text-start text-xs p-2 text-slate-100 rounded'>WHY US</button>
+        <button className='w-full border border-opacity-20 border-slate-200 text-start text-xs p-2 text-slate-100 rounded'> PROFILE</button>
       </div>
       <div className='justify-bottom mt-20'>
-      <button className='w-full text-start text-sm p-2 text-red-500 border border-red-500 rounded'>Log Out</button>
+      <button className='w-full text-start text-xs p-2 text-red-500 border border-red-500 rounded'>Log Out</button>
       </div>
     </div>
   )
