4cc8a27d282f7b56728a5c8e0ea128f88400113a
SukritiR29 <sukritirajora@gmail.com>
2024-03-27 22:37:05 +0530

    nav

--- components/nav.jsx
+++ components/nav.jsx
@@ -17,7 +17,7 @@ const Nav = () => {
         }
       }, [session, status]);
   return (
-    < div className='w-screen pt-2 border border-slate-400 bg-gray-950 border-opacity-20 flex pb-2 flex justify-between'>
+    < div className='w-full pt-2 border border-slate-400 bg-gray-950 border-opacity-20 flex pt-4 pb-4 flex justify-between'>
         <h1 className=' pl-6 text-sm font-bold bg-gradient-to-r  from-pink-500  to-yellow-500  text-transparent bg-clip-text uppercase'>Compassion Coin</h1>
       <div className='flex'>
       <RxAvatar className='text-md text-slate-200 mr-2'/>
