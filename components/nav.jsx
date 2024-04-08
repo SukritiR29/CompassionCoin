ad05051cf3ec2427dc63338a5df7083cac8822f6
SukritiR29 <sukritirajora@gmail.com>
2024-04-08 22:23:46 +0530

    nav

--- components/nav.jsx
+++ components/nav.jsx
@@ -17,11 +17,11 @@ const Nav = () => {
         }
       }, [session, status]);
   return (
-    < div className='fixed w-full pt-2 border border-slate-400 bg-gray-950 border-opacity-20 flex pt-4 pb-4 flex justify-between'>
-        <h1 className=' pl-6 text-sm font-bold bg-gradient-to-r  from-pink-500  to-yellow-500  text-transparent bg-clip-text uppercase'>Compassion Coin</h1>
+    < div className='fixed w-full pt-2  text-yellow-500  bg-blue-950 shadow flex pt-3 pb-3 flex justify-between'>
+        <h1 className=' pl-6 text-sm font-bold  uppercase '>Compassion Coin</h1>
       <div className='flex'>
-      <RxAvatar className='text-md text-slate-200 mr-2'/>
-      <h1 className='mr-10 text-sm font-semibold text-slate-200 uppercase'>{userName}</h1>
+      <RxAvatar className='text-md text-stone-200 mr-2'/>
+      <h1 className='mr-10 text-sm font-semibold text-stone-200 uppercase'>{userName}</h1>
       </div>
         
     </div>
@@ -29,3 +29,5 @@ const Nav = () => {
 }
 
 export default Nav
+
+//bg-gradient-to-r  from-pink-500  to-yellow-600  text-transparent bg-clip-text
